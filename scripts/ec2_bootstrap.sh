725c0d8de75a67727dfb6ead0e33690a7933ec41
Jerome Marc <root@ansible-tower-31.mbu.labs.redhat.com>
2017-04-05 17:02:49 -0400

    Fixes

--- scripts/ec2_bootstrap.sh
+++ scripts/ec2_bootstrap.sh
@@ -6,32 +6,21 @@
 # Enabled pipelining for Ansible
 /usr/bin/perl -ni -e 'print unless /^Defaults \s+ requiretty/x' /etc/sudoers
 
-# Tower callback
-#domain=${DOMAIN:-bovine.cow}
-#tower=$(dig +short _cm._tcp.${domain} srv | awk '/^0/ {print $4}')
-tower=<tower>
-#request=($(dig +short ${domain} txt | tr -d '"'))
-#template_key=${request[0]}
-template_key=<template_key>
-#template_id=${request[1]}
-template_id=<template_id>
+limit=`wget -qO- http://instance-data/latest/meta-data/public-ipv4`
+param=`echo {\"limit\": \"$limit\"}`
 
-if [[ -z ${tower} ]]; then
-    logger 'ansible: could not find tower host'
-    exit 1
-fi
+# Tower callback (via SSH forwarding):
+tower=52.91.21.15
+tower_user=admin
+tower_password=password
+template_key=0c3df5340427a69d4b6bcbb87e1b7282
+template_id=1944
 
-retry_attempts=10
-attempt=0
-while [[ $attempt -lt $retry_attempts ]]
-do
-  status_code=`curl -s -i --data "host_config_key=$template_key" http://$tower/api/v1/job_templates/$template_id/callback/ | head -n 1 | awk '{print $2}'`
-  if [[ $status_code == 202 ]]
-    then
-    exit 0
-  fi
-  attempt=$(( attempt + 1 ))
-  logger "bootstrap: ${status_code} received... retrying in 30 seconds (Attempt ${attempt})"
-  sleep 30
-done
+#if [[ -z ${tower} ]]; then
+#    logger 'ansible: could not find tower host'
+#    exit 1
+#fi
+
+echo "curl -f -k -H 'Content-Type: application/json' -XPOST -d '$param' --user $tower_user:$tower_password https://$tower:443/api/v1/job_templates/$template_id/launch/" | bash -
+
 exit 1
